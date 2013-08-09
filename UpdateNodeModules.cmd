263680ed96ab33606378458f80e3b6d76e066968
Guillaume Meyer <guillaumemeyerlive@gmail.com>
2013-08-09 13:27:17 +0200

    Added batch to update node modules

---
+++ UpdateNodeModules.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+npm install -g grunt-cli --save-dev
+npm install grunt-contrib-jshint --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-watch --save-dev
+npm install grunt-contrib-concat --save-dev
+npm install grunt-image-embed --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-commands --save-dev
+npm install grunt-contrib-yuidoc --save-dev
